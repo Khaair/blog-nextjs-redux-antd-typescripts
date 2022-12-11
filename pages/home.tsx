fd59d357961000bb255a4fb5f6ff9ae78ae5e031
Khaair <khair.cse.iu@gmail.com>
2022-12-11 13:25:56 +0600

    post delete,edit,search done

--- pages/home.tsx
+++ pages/home.tsx
@@ -9,25 +9,6 @@ export default function Home() {
           <div>
             <SearchBar />
           </div>
-          <h1 className="blog-head-title">BD News Portal</h1>
-          <p>
-            In other words, the digital journal. Maybe you want to document life
-            as a mother, your fashion sense, your adventures as your travel, or
-            just life in general, blogging is one of the easiest ways to get
-            your thoughts out there to the masses. (Of course you can always
-            blog privately if you’d rather just document your life for your own
-            personal reflection.) I love seeing how many people are finally
-            exploring the world of blogging and the community of inspiring and
-            talented individuals that populate it. However, it’s become a larger
-            discussion on whether it’s okay to document your kids on such
-            platforms since they can’t really give consent as babies, but
-            personally if I could go back and read a blog by my own mother as a
-            young mom that would be the most beautiful and priceless gift. And
-            one of the best things about blogging is there is no limit on how
-            long or what you can document. It will be interesting to see if this
-            generation will still be blogging, in whatever form that takes on,
-            in the next 50 years.
-          </p>
         </Col>
       </Row>
     </>
--- pages/searchBar.tsx
+++ pages/searchBar.tsx
@@ -5,6 +5,7 @@ import { fetchComments } from "../state-management/actions/comments";
 import { fetchUsers } from "../state-management/actions/users";
 import { fetchPosts } from "../state-management/actions/posts";
 import PostCard from "./postCard";
+import { Card } from "antd";
 
 function SearchBar({
   posts,
@@ -45,32 +46,78 @@ function SearchBar({
   };
 
   return (
-    <div>
-      <div className="inputsearch-area">
-        <input
-          type="text"
-          className="inputsearch"
-          placeholder="Search post here.."
-          value={wordEntered}
-          onChange={handleFilter}
-        />
-      </div>
-
-      {filteredData.length != 0 && (
-        <div className="dataResult">
-          {filteredData.slice(0, 15).map((item, index) => {
-            return (
-              <>
-                <div key={index} className="searchres">
-                  <h3>{item?.title} </h3>
-                  <p>{item?.body}</p>
+    <>
+      <div className="searchbar-area">
+        <div className="row">
+          <div className="col-lg-12">
+            <div>
+              <div className="inputsearch-area">
+                <input
+                  type="text"
+                  className="inputsearch"
+                  placeholder="Search post here.."
+                  value={wordEntered}
+                  onChange={handleFilter}
+                />
+              </div>
+              {filteredData.length > 0 ? (
+                <div className="dataResult">
+                  {filteredData.slice(0, 15).map((item, index) => {
+                    return (
+                      <>
+                        <div className="searchres-detais-area mt-3">
+                          <div className="container">
+                            <div className="row">
+                              <div className="col-lg-12">
+                                <Card>
+                                  <div key={index} className="searchres">
+                                    <h3>{item?.title} </h3>
+                                    <p>{item?.body}</p>
+                                  </div>
+                                </Card>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </>
+                    );
+                  })}
                 </div>
-              </>
-            );
-          })}
+              ) : (
+                <div className="banner-title-area">
+                  <div className="row">
+                    <div className="col-lg-12">
+                      <h1 className="blog-head-title">BD News Portal</h1>
+                      <p>
+                        In other words, the digital journal. Maybe you want to
+                        document life as a mother, your fashion sense, your
+                        adventures as your travel, or just life in general,
+                        blogging is one of the easiest ways to get your thoughts
+                        out there to the masses. (Of course you can always blog
+                        privately if you’d rather just document your life for
+                        your own personal reflection.) I love seeing how many
+                        people are finally exploring the world of blogging and
+                        the community of inspiring and talented individuals that
+                        populate it. However, it’s become a larger discussion on
+                        whether it’s okay to document your kids on such
+                        platforms since they can’t really give consent as
+                        babies, but personally if I could go back and read a
+                        blog by my own mother as a young mom that would be the
+                        most beautiful and priceless gift. And one of the best
+                        things about blogging is there is no limit on how long
+                        or what you can document. It will be interesting to see
+                        if this generation will still be blogging, in whatever
+                        form that takes on, in the next 50 years.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 }
 const mapStateToProps = (state: { posts: any; comments: any; users: any }) => {
